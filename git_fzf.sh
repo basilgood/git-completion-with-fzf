84cf4e4f494bdf3399bfa34cccd7dd3aa6ac391d
vasile luta <elsile69@yahoo.com>
2020-05-05 09:25:47 +0300

    feat: history commands

--- git_fzf.sh
+++ git_fzf.sh
@@ -108,9 +108,38 @@ gp() {
     awk '{print $2}'
 }
 
-gg() {
-  _z -l 2>&1 | fzf --height 40% --nth 2.. --reverse --inline-info --tac | sed 's/^[0-9,.]* *//'
-}
+__fzf_history() {
+  builtin history -n;
+  builtin typeset \
+    READLINE_LINE_NEW="$(
+      HISTTIMEFORMAT= builtin history |
+        command fzf +s --tac +m -n2..,.. --tiebreak=index --toggle-sort=ctrl-r |
+        command sed '
+              /^ *[0-9]/ {
+              s/ *\([0-9]*\) .*/!\1/;
+              b end;
+            };
+          d;
+          : end
+          '
+          )";
+
+          if
+            [[ -n $READLINE_LINE_NEW ]]
+          then
+            builtin bind '"\er": redraw-current-line'
+            builtin bind '"\e^": magic-space'
+            READLINE_LINE=${READLINE_LINE:+${READLINE_LINE:0:READLINE_POINT}}${READLINE_LINE_NEW}${READLINE_LINE:+${READLINE_LINE:READLINE_POINT}}
+            READLINE_POINT=$(( READLINE_POINT + ${#READLINE_LINE_NEW} ))
+          else
+            builtin bind '"\er":'
+            builtin bind '"\e^":'
+          fi
+        }
+
+builtin set -o histexpand;
+builtin bind -x '"\C-x1": __fzf_history';
+builtin bind '"\C-r": "\C-x1\e^\er"'
 
 if [[ $- =~ i ]]; then
   bind '"\er": redraw-current-line'
